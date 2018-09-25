d6a831fdc7f2fcbb741d779372352a38b587b03b
caleb <caleb.easterly@gmail.com>
2018-09-25 14:16:34 -0500

    add test for parents

--- tests/testTaxonomyAnalysis.py
+++ tests/testTaxonomyAnalysis.py
@@ -18,7 +18,8 @@ class TestTaxonomyAnalysis(unittest.TestCase):
         tax = testfile('simple_tax.tab')
         int = testfile('simple_int.tab')
         out = testfile('taxonomy_write_simple.tab')
-        metaquant_runner(mode='tax', sinfo='{"samp1": ["int"]}', int_file=int, tax_file=tax, tax_colname='lca', outfile=out)
+        metaquant_runner(mode='tax', sinfo='{"samp1": ["int"]}',
+                         int_file=int, tax_file=tax, tax_colname='lca', outfile=out)
         written = pd.read_table(out)
         self.assertAlmostEqual(written.query("taxon_name == 'Clostridioides difficile'")['samp1_mean'].values[0], np.log2(200))
 
@@ -39,6 +40,23 @@ class TestTaxonomyAnalysis(unittest.TestCase):
         # also, make sure firmicutes phylum is sum of c difficile and clostridiaceae, divided by all phyla
         self.assertEqual(tax_df['int1'][1239], np.log2(1020))
 
+    def testParentIntensityHigher(self):
+        """
+        make sure that parents always have higher intensity than children
+        """
+        tax=testfile('test_root_sum_uni.tab')
+        int=testfile('test_root_sum_int.tab')
+        tax_df = metaquant_runner('tax', sinfo='{"A": ["int"]}', pep_colname='peptide',
+                                  int_file=int, tax_file=tax, tax_colname='taxon_id',
+                                  min_peptides=1, min_children_non_leaf=1)
+        # filter to phylum and below
+        tax_df_filt = tax_df[(tax_df["rank"] != 'no rank') & (tax_df["rank"] != 'superkingdom')]
+        # firmicutes phylum should be highest
+        ints = tax_df_filt['int']
+        self.assertEqual(ints.max(), ints[1239])
+        # strep genus intensity should be greater than or equal to that of strep species
+        self.assertGreaterEqual(ints[1301], ints[1302])
+        self.assertGreaterEqual(ints[1301], ints[1305])
 
 if __name__ == '__main__':
     unittest.main()
