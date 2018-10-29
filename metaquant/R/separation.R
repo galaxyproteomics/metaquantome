b18e49da7abdfe424a305b6b6616cc5bc13f929e
caleb <caleb.easterly@gmail.com>
2018-10-29 16:04:16 -0500

    skeleton for r visualizations

---
+++ metaquant/R/separation.R
@@ -0,0 +1,17 @@
+
+# calculate the avg distance between n clusters
+# divided by the total variation within clusters
+sep_n <- function(clust){
+    nclust <- length(clust)
+    means <- lapply(clust, colMeans)
+    possible_dists <- combn(1:nclust, 2)
+    n_possible_dists <- ncol(possible_dists)
+    dists <- rep(0, n_possible_dists)
+    for (i in 1:n_possible_dists){
+        comb <- possible_dists[, i]
+        dists[i] <- sum((means[[comb[1]]] - means[[comb[2]]])^2)
+    }
+    avg_dist <- mean(dists)
+    sum_within <- sapply(1:nclust, function(i) mean((clust[[i]] - means[[i]])^2))
+    avg_dist / sum(sum_within)
+}
