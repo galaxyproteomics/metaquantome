0dfdc3c67c83f21bac59f3852c9cdd56d175977e
caleb <caleb.easterly@gmail.com>
2018-11-29 11:35:45 -0600

    move cli.py inside main metaquantome module

--- tests/travis/testCLI.py
+++ tests/travis/testCLI.py
@@ -10,7 +10,7 @@ from metaquantome.util.testutils import testfile, TTEST_SINFO
 class TestCLI(unittest.TestCase):
     def testSingleInt(self):
         out = testfile('cli_out.tab')
-        command = '''python3 cli.py expand -m t --pep_colname_int peptide --pep_colname_tax peptide ''' +\
+        command = '''python3 metaquantome/cli.py expand -m t --pep_colname_int peptide --pep_colname_tax peptide ''' +\
             '''--outfile ''' + out
         command += ''' -i metaquantome/data/test/simple_int.tab --tax_file metaquantome/data/test/simple_tax.tab '''
         command += '''--tax_colname "lca" --samps '{"A": ["int"]}' '''
@@ -24,7 +24,7 @@ class TestCLI(unittest.TestCase):
 
     def testMultipleInt(self):
         exp_out = testfile('cli_mult_out.tab')
-        exp_command = '''python3 cli.py expand -m f --pep_colname_int peptide --pep_colname_func peptide ''' +\
+        exp_command = '''python3 metaquantome/cli.py expand -m f --pep_colname_int peptide --pep_colname_func peptide ''' +\
             '''--outfile ''' + exp_out
         exp_command += ''' -i metaquantome/data/test/int_ttest.tab --func_file metaquantome/data/test/multiple_func.tab '''
         exp_command += ''' --func_colname cog --ontology cog ''' + " --samps '" + TTEST_SINFO + "' "
@@ -33,7 +33,7 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(exp_status, 0)
 
         test_out = testfile('cli_mult_test_out.tab')
-        test_command = "python3 cli.py stat -m f --outfile " + test_out + ' --file ' + exp_out
+        test_command = "python3 metaquantome/cli.py stat -m f --outfile " + test_out + ' --file ' + exp_out
         test_command += ''' --ontology cog ''' + " --samps '" + TTEST_SINFO + "'" + ' --parametric True '
         test_status = subprocess.call(test_command, shell=True)
         self.assertEqual(test_status, 0)
