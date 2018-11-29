d0822fa24c773c252fcc97eec811281dc3eaa494
caleb <caleb.easterly@gmail.com>
2018-11-29 02:02:03 -0600

    re-add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+VERSION='0.99.1'
+URL='https://github.com/galaxyproteomics/metaquantome'
+AUTHOR = 'Caleb Easterly'
+AUTHOR_EMAIL = 'caleb.easterly@gmail.com'
+setup(
+    name='metaquantome',
+    version=VERSION,
+    packages=['metaquantome'],
+    package_data={'metaquantome': ['data/*']},
+    license='Apache License 2.0',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description='Quantitative microbiome analysis',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Science/Research'
+    ],
+    install_requires=[
+        'pandas',
+        'ete3',
+        'goatools',
+        'numpy',
+        'statsmodels',
+        'biopython'
+    ],
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': ['metaquantome=metaquantome.cli:cli'],
+    },
+    project_urls={
+        "Bug Tracker": "https://github.com/galaxyproteomics/metaquantome/issues",
+        "Source Code": "https://github.com/galaxyproteomics/metaquantome",
+    }
+)
