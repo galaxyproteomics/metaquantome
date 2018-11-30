1e2a712c6bbc9e132e581ee5ab8e75b72cf39244
caleb <caleb.easterly@gmail.com>
2018-11-30 01:53:59 -0600

    version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION='0.99.1'
+VERSION='0.99.2'
 URL='https://github.com/galaxyproteomics/metaquantome'
 AUTHOR = 'Caleb Easterly'
 AUTHOR_EMAIL = 'caleb.easterly@gmail.com'
