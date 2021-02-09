1992e9672ac055d889a0113641d7ea493ee3a609
Jim Johnson <jj@umn.edu>
2021-02-09 16:14:41 -0600

    Update setup.py
    Update version to 2.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 URL = 'https://github.com/galaxyproteomics/metaquantome'
 AUTHOR = 'Caleb Easterly'
 AUTHOR_EMAIL = 'caleb.easterly@gmail.com'
